ef4b8c03a9297e9e5eb0fc476da457defec2baed
Evan Ovadia <verdagon@gmail.com>
2022-07-23 18:32:43 -0400

    Fixed drop for generics

--- Frontend/TypingPass/src/dev/vale/typing/macros/citizen/InterfaceDropMacro.scala
+++ Frontend/TypingPass/src/dev/vale/typing/macros/citizen/InterfaceDropMacro.scala
@@ -2,8 +2,8 @@ package dev.vale.typing.macros.citizen
 
 import dev.vale.highertyping.{FunctionA, InterfaceA}
 import dev.vale.postparsing.patterns.{AbstractSP, AtomSP, CaptureS}
-import dev.vale.postparsing.rules.{LookupSR, RuneUsage}
-import dev.vale.{Interner, Keywords, RangeS, StrI}
+import dev.vale.postparsing.rules.{CallSR, IRulexSR, LookupSR, RuneUsage}
+import dev.vale.{Accumulator, Interner, Keywords, RangeS, StrI}
 import dev.vale.postparsing._
 import dev.vale.typing.ast.PrototypeT
 import dev.vale.typing.env.{FunctionEnvEntry, IEnvEntry}
@@ -15,11 +15,12 @@ import dev.vale.highertyping.FunctionA
 import dev.vale.parsing.ast.MoveP
 import dev.vale.postparsing._
 import dev.vale.postparsing.patterns.AbstractSP
-import dev.vale.postparsing.rules.LookupSR
 import dev.vale.typing.env.IEnvironment
 import dev.vale.typing.names.FunctionTemplateNameT
 import dev.vale.typing.types._
 import dev.vale.typing.OverloadResolver
+
+import scala.collection.mutable
 
 class InterfaceDropMacro(
   interner: Interner,
@@ -29,37 +30,52 @@ class InterfaceDropMacro(
 
   val macroName: StrI = keywords.DeriveInterfaceDrop
 
-  override def getInterfaceSiblingEntries(structName: FullNameT[INameT], interfaceA: InterfaceA): Vector[(FullNameT[INameT], FunctionEnvEntry)] = {
-    Vector()
-  }
+  override def getInterfaceSiblingEntries(interfaceName: FullNameT[INameT], interfaceA: InterfaceA): Vector[(FullNameT[INameT], FunctionEnvEntry)] = {
+    def range(n: Int) = RangeS.internal(interner, n)
+    def use(n: Int, rune: IRuneS) = RuneUsage(range(n), rune)
 
-  override def getInterfaceChildEntries(interfaceName: FullNameT[INameT], interfaceA: InterfaceA, mutability: MutabilityT): Vector[(FullNameT[INameT], IEnvEntry)] = {
+    val v = CodeRuneS(keywords.V)
+    val t = CodeRuneS(keywords.T)
+    val self = CodeRuneS(keywords.self)
+
+    val runeToType = mutable.HashMap[IRuneS, ITemplataType]()
+    runeToType.put(t, CoordTemplataType())
+    runeToType.put(v, CoordTemplataType())
+
+    val rules = new Accumulator[IRulexSR]()
+    rules.add(LookupSR(range(-1672147),use(-64002, v),interner.intern(CodeNameS(keywords.void))))
+
+    val interfaceNameRune = StructNameRuneS(interfaceA.name)
+    runeToType += (interfaceNameRune -> interfaceA.tyype)
+    runeToType += (self -> CoordTemplataType())
+    rules.add(LookupSR(interfaceA.name.range, RuneUsage(interfaceA.name.range, interfaceNameRune), interfaceA.name.getImpreciseName(interner)))
+    rules.add(CallSR(interfaceA.name.range, use(-64002, self), RuneUsage(interfaceA.name.range, interfaceNameRune), interfaceA.genericParameters.map(_.rune).toArray))
+
     val dropFunctionA =
       FunctionA(
         interfaceA.name.range,
         interner.intern(FunctionNameS(keywords.drop, interfaceA.name.range.begin)),
         Vector(),
         TemplateTemplataType(Vector(CoordTemplataType()), FunctionTemplataType()),
-        Vector(
-          GenericParameterS(
-            RuneUsage(RangeS.internal(interner, -64002), CodeRuneS(keywords.T)),
-            None)),
-        Map(CodeRuneS(keywords.T) -> CoordTemplataType(), CodeRuneS(keywords.V) -> CoordTemplataType()),
+        Vector(GenericParameterS(use(-64002, t), None)),
+        runeToType.toMap,
         Vector(
           ParameterS(
             AtomSP(
-              RangeS.internal(interner, -1340),
+              range(-1340),
               Some(CaptureS(interner.intern(CodeVarNameS(keywords.thiss)))),
-              Some(AbstractSP(RangeS.internal(interner, -64002), true)),
-              Some(RuneUsage(RangeS.internal(interner, -64002), CodeRuneS(keywords.T))), None))),
-        Some(RuneUsage(RangeS.internal(interner, -64002), CodeRuneS(keywords.V))),
-        Vector(
-          LookupSR(RangeS.internal(interner, -1672146),RuneUsage(RangeS.internal(interner, -64002), CodeRuneS(keywords.T)),interner.intern(SelfNameS())),
-          LookupSR(RangeS.internal(interner, -1672147),RuneUsage(RangeS.internal(interner, -64002), CodeRuneS(keywords.V)),interner.intern(CodeNameS(keywords.void)))),
+              Some(AbstractSP(range(-64002), true)),
+              Some(use(-64002, self)), None))),
+        Some(use(-64002, v)),
+        rules.buildArray().toVector,
         AbstractBodyS)
 
     Vector(
-      interfaceName.addStep(nameTranslator.translateFunctionNameToTemplateName(dropFunctionA.name)) ->
+      interfaceName.copy(last = nameTranslator.translateFunctionNameToTemplateName(dropFunctionA.name)) ->
         FunctionEnvEntry(dropFunctionA))
+  }
+
+  override def getInterfaceChildEntries(interfaceName: FullNameT[INameT], interfaceA: InterfaceA, mutability: MutabilityT): Vector[(FullNameT[INameT], IEnvEntry)] = {
+    Vector()
   }
 }
